4913e63cf95fc47d5c3b589f9bd16afb69c38034
Arvind <arvind.vyas@supernova.xyz>
2019-01-09 14:57:39 +0530

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,7 +34,7 @@ module ApplicationHelper
 
 def set_leave_dates(leave)
   if leave.leave_date_from.present? && leave.leave_date_from.present?
-    "#{@leave.leave_date_from.strftime("%d/%m/%Y")} - #{@leave.leave_date_to.strftime("%d/%m/%Y")}"
+    "#{@leave.leave_date_from.to_date.strftime("%d/%m/%Y")} - #{@leave.leave_date_to.to_date.strftime("%d/%m/%Y")}"
   else
     ""
   end
